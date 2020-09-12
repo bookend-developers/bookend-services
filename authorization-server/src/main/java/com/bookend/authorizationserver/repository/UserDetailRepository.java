bd8f0c6f99e2d448df4c4c25eca1c9ff9b9f92be
ekremogr365 <eozturkeozturk@ogr365.iyte.edu.tr>
2020-09-12 17:15:16 +0300

    Initialization

---
+++ authorization-server/src/main/java/com/bookend/authorizationserver/repository/UserDetailRepository.java
@@ -0,0 +1,13 @@
+package com.bookend.authorizationserver.repository;
+
+import com.bookend.authorizationserver.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserDetailRepository extends JpaRepository<User,Integer> {
+
+
+    Optional<User> findByUsername(String name);
+
+}
---
+++ book-producer/src/test/java/com/bookend/bookproducer/BookProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookend.bookproducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
