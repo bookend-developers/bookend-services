7b14443cc7b8293c682c67064ec886efa771a17c
eltrhenu <37040918+eltrhenu@users.noreply.github.com>
2020-10-03 15:10:03 +0300

    old account service is deleted

--- accountservice/src/main/java/com/bookend/shelfservice/service/BookService.java
+++
@@ -1,9 +0,0 @@
-package com.bookend.shelfservice.service;
-
-import com.bookend.shelfservice.model.Book;
-
-public interface BookService {
-    Book getById(String id);
-
-    public Book saveOrUpdate(Book book);
-}
