603566a0bf8d53b7d750770f79d50bcd36a451be
eltrhenu <37040918+eltrhenu@users.noreply.github.com>
2020-10-15 20:08:16 +0300

    api swagger attempt

--- author-service/src/main/java/com/bookend/authorservice/model/Book.java
+++ author-service/src/main/java/com/bookend/authorservice/model/Book.java
@@ -1,5 +1,6 @@
 package com.bookend.authorservice.model;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "books")
@@ -7,69 +8,36 @@ public class Book {
     @Id
 
     private String id;
-    private Integer page;
-    //TODO make it enum
-    private String genre;
-
-    private String description;
-
-    private String bookName;
-    private String author;
+    private String bookId;
+    @DBRef
+    private Author author;
 
     public String getId() {
         return id;
     }
 
-    public String getBookName() {
-        return bookName;
+
+    public String getBookId() {
+        return bookId;
     }
 
-    public void setBookName(String bookName) {
-        this.bookName = bookName;
+    public void setBookId(String bookId) {
+        this.bookId = bookId;
     }
 
-    public String getAuthor() {
-        return this.author;
+    public Author getAuthor() {
+        return author;
     }
 
-    public void setAuthor(String author) {
+    public void setAuthor(Author author) {
         this.author = author;
     }
-
-    public Integer getPage() {
-        return page;
-    }
-
-    public void setPage(Integer page) {
-        this.page = page;
-    }
-
-    public String getGenre() {
-        return genre;
-    }
-
-    public void setGenre(String genre) {
-        this.genre = genre;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-
 
     public Book() {
     }
 
-    public Book(Integer page, String genre, String description, String bookName, String author) {
-        this.page = page;
-        this.genre = genre;
-        this.description = description;
-        this.bookName = bookName;
+    public Book(String bookId, Author author) {
+        this.bookId = bookId;
         this.author = author;
     }
 }
