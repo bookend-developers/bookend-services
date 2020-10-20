ef46e3b801d50cbcfe8e640c1279d80544352ecf
eltrhenu <37040918+eltrhenu@users.noreply.github.com>
2020-10-20 15:41:49 +0300

    api customization

--- rate-comment-service/src/main/java/com/ratecommentservice/controller/RatingController.java
+++ rate-comment-service/src/main/java/com/ratecommentservice/controller/RatingController.java
@@ -3,9 +3,14 @@ package com.ratecommentservice.controller;
 
 import com.ratecommentservice.model.Rate;
 import com.ratecommentservice.service.RateService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -22,36 +27,46 @@ public class RatingController {
     }
 
 
-    //TODO do we really need book's all rates
-    @GetMapping("/{bookId}")
-    public List<Rate> getBookRates(@PathVariable("bookId") String  bookId ){
 
-        return rateService.getBookRates(bookId);
-
-
-    }
-
+    @ApiOperation(value = "Get book's average rate value ", response = Double.class)
     @GetMapping("/book/{bookId}")
     public Double getBookRate(@PathVariable("bookId") String  bookId ){
+
         return rateService.getBookAverageRate(bookId);
 
 
     }
-
+    @ApiOperation(value = "Get current user's rates ", response = Rate.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successfully retrieved list"),
+            @ApiResponse(code = 401, message = "You are not authorized to view the resource"),
+    }
+    )
     @GetMapping("/user")
     public List<Rate> getUserRates( OAuth2Authentication auth) {
+
         return rateService.getUserRates(auth.getName());
 
 
     }
+    @ApiOperation(value = "Rate a specific book", response = Rate.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successfully retrieved rate"),
+            @ApiResponse(code = 401, message = "You are not authorized to rate the boook"),
+            @ApiResponse(code = 400, message = "The way you are trying to rate is not accepted.")
+    }
+    )
     @PostMapping("/new/{bookid}")
     public Rate rateBook(OAuth2Authentication auth
             ,@PathVariable("bookid") String  bookId
             ,@RequestParam("rate") Double rate){
-        //Rate newRate = new Rate(bookId,auth.getName(),rate);
+        if(rate== null){
+            throw  new ResponseStatusException(HttpStatus.BAD_REQUEST,"Please fill all necessary places");
+        }
         return rateService.save( new Rate(bookId,auth.getName(),rate));
 
     }
+    @ApiOperation(value = "Delete a specific rate")
     @DeleteMapping("/delete/{rateid}")
     public void deleteRate(@PathVariable("rateid") String rateId){
         rateService.deleteRate(Long.valueOf(rateId));
