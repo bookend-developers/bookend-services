4ed7467ddd2272b732c58ca02a95a16bcd9ed695
eltrhenu <37040918+eltrhenu@users.noreply.github.com>
2020-10-28 21:48:02 +0300

    minor changes

--- book-service/src/main/java/com/bookend/bookservice/service/BookService.java
+++ book-service/src/main/java/com/bookend/bookservice/service/BookService.java
@@ -3,9 +3,11 @@ package com.bookend.bookservice.service;
 import com.bookend.bookservice.model.Book;
 
 import java.util.List;
+import java.util.Map;
 
 public interface BookService {
     Book getById(String id);
+    Map<String,String> getFullBookById(String id, String accessToken);
     Book saveOrUpdate(Book book);
     List<Book> getBooksofShelf(Long shelfID,String accessToken);
     List<Book> getAll();
