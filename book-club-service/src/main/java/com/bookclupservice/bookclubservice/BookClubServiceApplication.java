e055a304cb9e020af556fa612eb0a7f5860c6443
ekremogr365 <68231424+ekremogr365@users.noreply.github.com>
2020-11-28 16:14:13 +0300

    user çekme

--- book-club-service/src/main/java/com/bookclupservice/bookclubservice/BookClubServiceApplication.java
+++ book-club-service/src/main/java/com/bookclupservice/bookclubservice/BookClubServiceApplication.java
@@ -2,6 +2,7 @@ package com.bookclupservice.bookclubservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
