0c693851c238cdf7992b6c61d849a5c03480b6f8
ekremoztrk <68231424+ekremoztrk@users.noreply.github.com>
2022-03-24 19:39:13 +0300

    Update mutationChoicer.py

--- mutationChoicer.py
+++ mutationChoicer.py
@@ -11,16 +11,16 @@ def get_random_service_and_function(list_of_services, num_of_services):
         list_of_services.remove(random_service)
         print("servis name:", random_service.name,"function_no", random.randint(1, random_service.num_of_functions))
 
-author_service = Service("author", 28)
-authorization_service = Service("authorization", 7)
-book_club_service = Service("book-club", 39)
-book_service = Service("book", 38)
-mail_service = Service("mail", 3)
-message_service = Service("message", 27)
-rate_comment_service = Service("rate-comment", 24)
-shelf_service = Service("shelf", 18)
+author_service = Service("author", 8)
+authorization_service = Service("authorization", 6)
+book_club_service = Service("book-club", 14)
+book_service = Service("book", 18)
+mail_service = Service("mail", 5)
+message_service = Service("message", 6)
+rate_comment_service = Service("rate-comment", 18)
+shelf_service = Service("shelf", 12)
 
 
 list_of_services = [author_service, authorization_service, book_club_service, book_service, mail_service, message_service, rate_comment_service, shelf_service]
 
-get_random_service_and_function(list_of_services, 2)
+get_random_service_and_function(list_of_services, 1)
